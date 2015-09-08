5b94f0fd1dd679192da4ed9eb93e38a44c05ccb3
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-09-08 10:24:46 +0200

    update

--- step.sh
+++ step.sh
@@ -25,7 +25,7 @@ of the app you try to deploy to iTunes Connect then you might want to upload the
 build manually to make sure it fulfills the initial iTunes Connect submission
 verification process."
 
-  if [[ "${STEP_DELIVER_DEPLOY_IS_SUBMIT_FOR_BETA}" == "yes" ]] ; then
+  if [[ "${submit_for_beta}" == "yes" ]] ; then
 	write_section_to_formatted_output "**Beta deply note:** you
 should try to disable the \`Submit for TestFlight Beta Testing\` option and try
 the deploy again."
@@ -42,28 +42,28 @@ set_error_cleanup_function CLEANUP_ON_ERROR_FN
 # ---------------------
 # --- Required Inputs
 
-if [ -z "${STEP_DELIVER_DEPLOY_IPA_PATH}" ] ; then
-	echo " [!] \`STEP_DELIVER_DEPLOY_IPA_PATH\` not provided!"
+if [ -z "${ipa_path}" ] ; then
+	echo " [!] \`ipa_path\` not provided!"
 	exit 1
 fi
 
-if [ -z "${STEP_DELIVER_DEPLOY_ITUNESCON_PASSWORD}" ] ; then
-	echo " [!] \`STEP_DELIVER_DEPLOY_ITUNESCON_PASSWORD\` not provided!"
+if [ -z "${password}" ] ; then
+	echo " [!] \`password\` not provided!"
 	exit 1
 fi
 
-if [ -z "${STEP_DELIVER_DEPLOY_ITUNESCON_USER}" ] ; then
-	echo " [!] \`STEP_DELIVER_DEPLOY_ITUNESCON_USER\` not provided!"
+if [ -z "${itunescon_user}" ] ; then
+	echo " [!] \`itunescon_user\` not provided!"
 	exit 1
 fi
 
-if [ -z "${STEP_DELIVER_DEPLOY_ITUNESCON_APP_ID}" ] ; then
-	echo " [!] \`STEP_DELIVER_DEPLOY_ITUNESCON_APP_ID\` not provided!"
+if [ -z "${app_id}" ] ; then
+	echo " [!] \`app_id\` not provided!"
 	exit 1
 fi
 
 CONFIG_testflight_beta_deploy_type_flag='--skip-deploy'
-if [[ "${STEP_DELIVER_DEPLOY_IS_SUBMIT_FOR_BETA}" == "yes" ]] ; then
+if [[ "${submit_for_beta}" == "yes" ]] ; then
 	CONFIG_testflight_beta_deploy_type_flag='--beta'
 fi
 
@@ -95,14 +95,14 @@ This means that when the API changes
 **this step might fail until the tool is updated**."
 
 
-export DELIVER_USER="${STEP_DELIVER_DEPLOY_ITUNESCON_USER}"
-export DELIVER_PASSWORD="${STEP_DELIVER_DEPLOY_ITUNESCON_PASSWORD}"
-deliver testflight ${CONFIG_testflight_beta_deploy_type_flag} -a "${STEP_DELIVER_DEPLOY_ITUNESCON_APP_ID}" "${STEP_DELIVER_DEPLOY_IPA_PATH}"
+export DELIVER_USER="${itunescon_user}"
+export DELIVER_PASSWORD="${password}"
+deliver testflight ${CONFIG_testflight_beta_deploy_type_flag} -a "${app_id}" "${ipa_path}"
 fail_if_cmd_error "Deploy failed!"
 
 write_section_to_formatted_output "# Success"
 echo_string_to_formatted_output "* The app (.ipa) was successfully uploaded to [iTunes Connect](https://itunesconnect.apple.com), you should see it in the *Prerelease* section on the app's iTunes Connect page!"
-if [[ "${STEP_DELIVER_DEPLOY_IS_SUBMIT_FOR_BETA}" != "yes" ]] ; then
+if [[ "${submit_for_beta}" != "yes" ]] ; then
 	echo_string_to_formatted_output "* **Don't forget to enable** the **TestFlight Beta Testing** switch on iTunes Connect (on the *Prerelease* tab of the app) if this is a new version of the app!"
 fi
 
