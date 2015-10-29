2910b235ab7ee1fae391eeea0b9c7cb4d7c3dd54
Viktor Benei <viktor.benei@gmail.com>
2015-10-29 12:36:00 +0100

    added support for OS X system ruby and brew installed ruby; is_expand: true for all inputs

--- _setup.sh
+++ _setup.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
 
-set -e
-
 command_exists () {
 	command -v "$1" >/dev/null 2>&1 ;
 }
 
-if command_exists deliver ; then
-	echo " (i) deliver already installed"
+gem_name="deliver"
+
+if command_exists $gem_name ; then
+	echo " (i) $gem_name already installed"
 	exit 0
 else
-	echo " (i) deliver NOT yet installed, attempting install..."
+	echo " (i) $gem_name NOT yet installed, attempting install..."
 fi
 
 STARTTIME=$(date +%s)
 
-if command_exists rvm ; then
+which_ruby="$(which ruby)"
+osx_system_ruby_pth="/usr/bin/ruby"
+brew_ruby_pth="/usr/local/bin/ruby"
+
+echo
+echo " (i) Which ruby: $which_ruby"
+echo " (i) Ruby version: $(ruby --version)"
+echo
+
+set -e
+
+if [[ "$which_ruby" == "$osx_system_ruby_pth" ]] ; then
+	echo " -> using system ruby - requires sudo"
+	echo '$' sudo gem install ${gem_name} --no-document
+	sudo gem install ${gem_name} --no-document
+elif [[ "$which_ruby" == "$brew_ruby_pth" ]] ; then
+	echo " -> using brew ($brew_ruby_pth) ruby"
+	echo '$' gem install ${gem_name} --no-document
+	gem install ${gem_name} --no-document
+elif command_exists rvm ; then
 	echo " -> installing with RVM"
-	gem install deliver
+	echo '$' gem install ${gem_name} --no-document
+	gem install ${gem_name} --no-document
 elif command_exists rbenv ; then
 	echo " -> installing with rbenv"
-	gem install deliver
+	echo '$' gem install ${gem_name} --no-document
+	gem install ${gem_name} --no-document
+	echo '$' rbenv rehash
 	rbenv rehash
 else
-	echo " [!] Failed to install: neither RVM nor rbenv is available!"
+	echo " [!] Failed to install: no ruby is available!"
 	exit 1
 fi
 
