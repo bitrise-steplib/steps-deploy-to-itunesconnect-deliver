546f7e0f80d1ae8a9d4ff65e901e81476343d8ba
Krisztián Gödrei <godreikrisztian@gmail.com>
2016-12-06 13:58:57 +0100

    go-toolkit support (#16)
    * go-toolkit support
    
    * bitrise.yml fix

---
+++ main.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/bitrise-io/go-utils/cmdex"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-io/steps-deploy-to-itunesconnect-deliver/rubycmd"
+	"github.com/kballard/go-shellquote"
+)
+
+// ConfigsModel ...
+type ConfigsModel struct {
+	IpaPath string
+	PkgPath string
+
+	ItunesconUser string
+	Password      string
+
+	AppID           string
+	SubmitForBeta   string
+	SkipMetadata    string
+	SkipScreenshots string
+	TeamID          string
+	TeamName        string
+	Options         string
+
+	UpdateDeliver string
+}
+
+func createConfigsModelFromEnvs() ConfigsModel {
+	return ConfigsModel{
+		IpaPath: os.Getenv("ipa_path"),
+		PkgPath: os.Getenv("pkg_path"),
+
+		ItunesconUser: os.Getenv("itunescon_user"),
+		Password:      os.Getenv("password"),
+
+		AppID:           os.Getenv("app_id"),
+		SubmitForBeta:   os.Getenv("submit_for_beta"),
+		SkipMetadata:    os.Getenv("skip_metadata"),
+		SkipScreenshots: os.Getenv("skip_screenshots"),
+		TeamID:          os.Getenv("team_id"),
+		TeamName:        os.Getenv("team_name"),
+		Options:         os.Getenv("options"),
+
+		UpdateDeliver: os.Getenv("update_deliver"),
+	}
+}
+
+func (configs ConfigsModel) print() {
+	log.Info("Configs:")
+
+	log.Detail("- IpaPath: %s", configs.IpaPath)
+	log.Detail("- PkgPath: %s", configs.PkgPath)
+
+	log.Detail("- ItunesconUser: %s", configs.ItunesconUser)
+
+	securePassword := ""
+	if configs.Password != "" {
+		securePassword = "***"
+	}
+	log.Detail("- Password: %s", securePassword)
+
+	log.Detail("- AppID: %s", configs.AppID)
+	log.Detail("- SubmitForBeta: %s", configs.SubmitForBeta)
+	log.Detail("- SkipMetadata: %s", configs.SkipMetadata)
+	log.Detail("- SkipScreenshots: %s", configs.SkipScreenshots)
+	log.Detail("- TeamID: %s", configs.TeamID)
+	log.Detail("- TeamName: %s", configs.TeamName)
+	log.Detail("- Options: %s", configs.Options)
+
+	log.Detail("- UpdateDeliver: %s", configs.UpdateDeliver)
+}
+
+func (configs ConfigsModel) validate() error {
+	if configs.IpaPath == "" && configs.PkgPath == "" {
+		return errors.New("no IpaPath not PkgPath parameter specified")
+	}
+
+	if configs.IpaPath != "" {
+		if exist, err := pathutil.IsPathExists(configs.IpaPath); err != nil {
+			return fmt.Errorf("failed to check if IpaPath exist at: %s, error: %s", configs.IpaPath, err)
+		} else if !exist {
+			return fmt.Errorf("IpaPath not exist at: %s", configs.IpaPath)
+		}
+	}
+
+	if configs.PkgPath != "" {
+		if exist, err := pathutil.IsPathExists(configs.PkgPath); err != nil {
+			return fmt.Errorf("failed to check if PkgPath exist at: %s, error: %s", configs.PkgPath, err)
+		} else if !exist {
+			return fmt.Errorf("PkgPath not exist at: %s", configs.PkgPath)
+		}
+	}
+
+	if configs.ItunesconUser == "" {
+		return errors.New("no ItunesconUser parameter specified")
+	}
+
+	if configs.Password == "" {
+		return errors.New("no Password parameter specified")
+	}
+
+	if configs.AppID == "" {
+		return errors.New("no AppID parameter specified")
+	}
+
+	if configs.SubmitForBeta == "" {
+		return errors.New("no SubmitForBeta parameter specified")
+	}
+
+	if configs.SkipMetadata == "" {
+		return errors.New("no SkipMetadata parameter specified")
+	}
+
+	if configs.SkipScreenshots == "" {
+		return errors.New("no SkipScreenshots parameter specified")
+	}
+
+	if configs.UpdateDeliver == "" {
+		return errors.New("no UpdateDeliver parameter specified")
+	}
+
+	return nil
+}
+
+func fail(format string, v ...interface{}) {
+	log.Error(format, v...)
+	os.Exit(1)
+}
+
+func main() {
+	configs := createConfigsModelFromEnvs()
+
+	fmt.Println()
+	configs.print()
+
+	if err := configs.validate(); err != nil {
+		fail("Issue with input: %s", err)
+	}
+
+	//
+	// Setup
+	fmt.Println()
+	log.Info("Setup")
+
+	startTime := time.Now()
+
+	rubyCmd, err := rubycmd.NewRubyCommandModel()
+	if err != nil {
+		fail("Failed to create ruby command model, error: %s", err)
+	}
+
+	deliverGemName := "deliver"
+	installed, err := rubyCmd.IsGemInstalled(deliverGemName, "")
+	if err != nil {
+		fail("Failed to check if gem (%s) installed, error: %s", err)
+	}
+
+	installDeliver := true
+	if installed {
+		log.Detail("%s already installed", deliverGemName)
+
+		if configs.UpdateDeliver == "no" {
+			log.Detail("update %s disabled, setup finished...", deliverGemName)
+			installDeliver = false
+		} else {
+			log.Detail("updating %s...", deliverGemName)
+		}
+	} else {
+		log.Detail("%s NOT yet installed, attempting install...")
+	}
+
+	if installDeliver {
+		rubyCmd.GemInstall(deliverGemName, "")
+	}
+
+	elapsed := time.Since(startTime)
+
+	log.Detail("Setup took %d secounds to complete", elapsed.Seconds)
+
+	//
+	// Main
+	fmt.Println()
+	log.Info("Deploy")
+
+	log.Detail(`**Note:** if your password
+contains special characters
+and you experience problems, please
+consider changing your password
+to something with only
+alphanumeric characters.`)
+	fmt.Println()
+
+	log.Detail(`**Be advised** log.Detail(that this
+step uses a well maintained, open source tool which
+uses *undocumented and unsupported APIs* (because the current
+iTunes Connect platform does not have a documented and supported API)
+to perform the deployment.
+This means that when the API changes
+**this step might fail until the tool is updated**.`)
+	fmt.Println()
+
+	options := []string{}
+	if configs.Options != "" {
+		opts, err := shellquote.Split(configs.Options)
+		if err != nil {
+			fail("Failed to split options (%s), error: %s", configs.Options, err)
+		}
+		options = opts
+	}
+
+	envs := []string{
+		fmt.Sprintf("DELIVER_PASSWORD=%s", configs.Password),
+	}
+
+	args := []string{
+		"--username", configs.ItunesconUser,
+		"--app", configs.AppID,
+	}
+
+	if configs.IpaPath != "" {
+		args = append(args, "--ipa", configs.IpaPath)
+	} else if configs.PkgPath != "" {
+		args = append(args, "--pkg", configs.PkgPath)
+	}
+
+	if configs.SkipScreenshots == "yes" {
+		args = append(args, "--skip_screenshots")
+	}
+
+	if configs.SkipMetadata == "yes" {
+		args = append(args, "--skip_metadata")
+	}
+
+	args = append(args, "--force")
+
+	if configs.SubmitForBeta == "yes" {
+		args = append(args, "--submit_for_review")
+	}
+
+	args = append(args, options...)
+
+	log.Done("$ %s", cmdex.PrintableCommandArgs(false, append([]string{"deliver"}, args...)))
+
+	cmd := cmdex.NewCommand("deliver", args...)
+
+	cmd.SetStdout(os.Stdout)
+	cmd.SetStderr(os.Stderr)
+	cmd.SetStdin(os.Stdin)
+	cmd.AppendEnvs(envs)
+
+	fmt.Println()
+
+	if err := cmd.Run(); err != nil {
+		fail("Deploy failed, error: %s", err)
+	}
+
+	log.Done("Success")
+	log.Detail("The app (.ipa) was successfully uploaded to [iTunes Connect](https://itunesconnect.apple.com), you should see it in the *Prerelease* section on the app's iTunes Connect page!")
+	log.Detail("**Don't forget to enable** the **TestFlight Beta Testing** switch on iTunes Connect (on the *Prerelease* tab of the app) if this is a new version of the app!")
+}
--- step.sh
+++ step.sh
@@ -1,149 +1,15 @@
 #!/bin/bash
-
+set -ex
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# load bash utils
-source "${THIS_SCRIPT_DIR}/bash_utils/utils.sh"
-source "${THIS_SCRIPT_DIR}/bash_utils/formatted_output.sh"
 
+tmp_gopath_dir="$(mktemp -d)"
 
-echo "Config:"
-echo " * ipa_path: ${ipa_path}"
-echo " * pkg_path: ${pkg_path}"
-echo " * itunescon_user: ${itunescon_user}"
-echo " * password: ***"
-echo " * app_id: ${app_id}"
-echo " * submit_for_beta: ${submit_for_beta}"
-echo " * skip_metadata: ${skip_metadata}"
-echo " * skip_screenshots: ${skip_screenshots}"
-echo " * team_id: ${team_id}"
-echo " * team_name: ${team_name}"
-echo " * update_deliver: ${update_deliver}"
+go_package_name="github.com/bitrise-io/steps-deploy-to-itunesconnect-deliver"
+full_package_path="${tmp_gopath_dir}/src/${go_package_name}"
+mkdir -p "${full_package_path}"
 
-# ------------------------------
-# --- Error Cleanup
+rsync -avh --quiet "${THIS_SCRIPT_DIR}/" "${full_package_path}/"
 
-function finalcleanup {
-  echo "-> finalcleanup"
-  local fail_msg="$1"
-
-  write_section_to_formatted_output "# Error"
-  if [ ! -z "${fail_msg}" ] ; then
-    write_section_to_formatted_output "**Error Description**:"
-    write_section_to_formatted_output "${fail_msg}"
-  fi
-  write_section_to_formatted_output "*See the logs for more information*"
-
-  write_section_to_formatted_output "**If this is the very first build**
-of the app you try to deploy to iTunes Connect then you might want to upload the first
-build manually to make sure it fulfills the initial iTunes Connect submission
-verification process."
-
-  if [[ "${submit_for_beta}" == "yes" ]] ; then
-	write_section_to_formatted_output "**Beta deploy note:** you
-should try to disable the \`Submit for TestFlight Beta Testing\` option and try
-the deploy again."
-  fi
-}
-
-function CLEANUP_ON_ERROR_FN {
-  local err_msg="$1"
-  finalcleanup "${err_msg}"
-}
-set_error_cleanup_function CLEANUP_ON_ERROR_FN
-
-
-# ---------------------
-# --- Required Inputs
-
-if [ -z "${ipa_path}" ] && [ -z "${pkg_path}" ] ; then
-	echo " [!] ipa/pkg path not provided!"
-	exit 1
-fi
-
-if [ -z "${password}" ] ; then
-	echo " [!] \`password\` not provided!"
-	exit 1
-fi
-
-if [ -z "${itunescon_user}" ] ; then
-	echo " [!] \`itunescon_user\` not provided!"
-	exit 1
-fi
-
-if [ -z "${app_id}" ] ; then
-	echo " [!] \`app_id\` not provided!"
-	exit 1
-fi
-
-
-CONFIG_package_type=''
-CONFIG_package_path=''
-if [ -n "${ipa_path}" ] ; then
-  CONFIG_package_type='--ipa'
-  CONFIG_package_path="${ipa_path}"
-elif [ -n "${pkg_path}" ] ; then
-  CONFIG_package_type='--pkg'
-  CONFIG_package_path="${pkg_path}"
-fi
-
-CONFIG_testflight_beta_deploy_type_flag=''
-if [[ "${submit_for_beta}" == "yes" ]] ; then
-	CONFIG_testflight_beta_deploy_type_flag='--submit_for_review'
-fi
-
-CONFIG_skip_metadata_type_flag=''
-if [[ "${skip_metadata}" == "yes" ]] ; then
-	CONFIG_skip_metadata_type_flag='--skip_metadata'
-fi
-
-CONFIG_skip_screenshots_type_flag=''
-if [[ "${skip_screenshots}" == "yes" ]] ; then
-	CONFIG_skip_screenshots_type_flag='--skip_screenshots'
-fi
-
-# ---------------------
-# --- Main
-
-write_section_to_formatted_output "# Setup"
-bash "${THIS_SCRIPT_DIR}/_setup.sh" ${update_deliver}
-fail_if_cmd_error "Failed to setup the required tools!"
-
-write_section_to_formatted_output "# Deploy"
-
-write_section_to_formatted_output "**Note:** if your password
-contains special characters
-and you experience problems, please
-consider changing your password
-to something with only
-alphanumeric characters."
-
-write_section_to_formatted_output "**Be advised** that this
-step uses a well maintained, open source tool which
-uses *undocumented and unsupported APIs* (because the current
-iTunes Connect platform does not have a documented and supported API)
-to perform the deployment.
-This means that when the API changes
-**this step might fail until the tool is updated**."
-
-export DELIVER_PASSWORD="${password}"
-
-if [ -n "${team_id}" ] ; then
-  set -x
-  deliver --username "${itunescon_user}" --app "${app_id}" --team_id "${team_id}" ${CONFIG_package_type} "${CONFIG_package_path}" ${CONFIG_skip_screenshots_type_flag} ${CONFIG_skip_metadata_type_flag} --force ${CONFIG_testflight_beta_deploy_type_flag}
-elif [ -n "${team_name}" ] ; then
-  set -x
-  deliver --username "${itunescon_user}" --app "${app_id}" --team_name "${team_name}" ${CONFIG_package_type} "${CONFIG_package_path}" ${CONFIG_skip_screenshots_type_flag} ${CONFIG_skip_metadata_type_flag} --force ${CONFIG_testflight_beta_deploy_type_flag}
-else
-  set -x
-  deliver --username "${itunescon_user}" --app "${app_id}" ${CONFIG_package_type} "${CONFIG_package_path}" ${CONFIG_skip_screenshots_type_flag} ${CONFIG_skip_metadata_type_flag} --force ${CONFIG_testflight_beta_deploy_type_flag}
-fi
-fail_if_cmd_error "Deploy failed!"
-set +x
-
-write_section_to_formatted_output "# Success"
-echo_string_to_formatted_output "* The app (.ipa) was successfully uploaded to [iTunes Connect](https://itunesconnect.apple.com), you should see it in the *Prerelease* section on the app's iTunes Connect page!"
-if [[ "${submit_for_beta}" != "yes" ]] ; then
-	echo_string_to_formatted_output "* **Don't forget to enable** the **TestFlight Beta Testing** switch on iTunes Connect (on the *Prerelease* tab of the app) if this is a new version of the app!"
-fi
-
-exit 0
+export GOPATH="${tmp_gopath_dir}"
+export GO15VENDOREXPERIMENT=1
+go run "${full_package_path}/main.go"
