252eb29e277708a50c181bbdf214b5170e096e81
trapacska <trapacska@gmail.com>
2017-01-24 22:30:56 +0100

    code clean-up

--- main.go
+++ main.go
@@ -254,7 +254,6 @@ This means that when the API changes
 		if configs.TeamID != "" {
 			log.Warnf("TeamName parameter specified, TeamID will be ignored")
 		}
-
 	} else if configs.TeamID != "" {
 		args = append(args, "--team_id", configs.TeamID)
 	}
