f8a4ae946beb2347e6aeb5174056b58ea451e2f4
Akos Birmacher <akos.birmacher@bitrise.io>
2019-03-22 13:04:50 +0100

    -

--- main.go
+++ main.go
@@ -277,8 +277,7 @@ alphanumeric characters.`)
 	fmt.Println()
 
 	log.Printf(`**Be advised**
-that this
-step uses a well maintained, open source tool which
+that this step uses a well maintained, open source tool which
 uses *undocumented and unsupported APIs* (because the current
 iTunes Connect platform does not have a documented and supported API)
 to perform the deployment.
