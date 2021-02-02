83b26d1a49a9306ad8793ed48bcb9236877d6a77
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2021-02-02 13:46:45 +0100

    Rename JWTConnection to APIKeyConnection and SessionConnection to AppleIDConnection.

--- main.go
+++ main.go
@@ -320,7 +320,7 @@ func main() {
 			handleSessionDataError(err)
 		}
 
-		if conn == nil || (conn.JWTConnection == nil && conn.SessionConnection == nil) {
+		if conn == nil || (conn.APIKeyConnection == nil && conn.AppleIDConnection == nil) {
 			fmt.Println()
 			log.Debugf("%s", notConnected)
 		}
