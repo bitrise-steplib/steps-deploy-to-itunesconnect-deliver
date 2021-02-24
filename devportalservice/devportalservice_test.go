c55aafe5df5a8802f7faf7b026000b29aebaeb3f
lpusok <7979773+lpusok@users.noreply.github.com>
2021-02-24 08:22:25 +0100

    Normalize test devices, adding comparator (#101)
    * Test devices are filtered to contain valid characters only. Duplicate test devices are filtered out.
    
    * Added IsEqualUDID to check the equality of two UDIDs, disregarding separators and casing differences.

--- devportalservice/devportalservice_test.go
+++ devportalservice/devportalservice_test.go
@@ -139,3 +139,101 @@ func newMockHTTPClient(response *http.Response, err error) mockHTTPClient {
 func (c mockHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	return c.response, c.err
 }
+
+func Test_validateTestDevice(t *testing.T) {
+	tests := []struct {
+		name                  string
+		deviceList            []TestDevice
+		wantValidDevices      []TestDevice
+		wantDuplicatedDevices []TestDevice
+	}{
+		{
+			name: "One device",
+			deviceList: []TestDevice{{
+				DeviceID: "612cb2257",
+			}},
+			wantValidDevices: []TestDevice{{
+				DeviceID: "612cb2257",
+			}},
+			wantDuplicatedDevices: nil,
+		},
+		{
+			name: "One device with whitespace",
+			deviceList: []TestDevice{{
+				DeviceID: "612c b2257 ",
+			}},
+			wantValidDevices: []TestDevice{{
+				DeviceID: "612cb2257",
+			}},
+			wantDuplicatedDevices: nil,
+		},
+		{
+			name: "One device with separator",
+			deviceList: []TestDevice{{
+				DeviceID: "00008020-00213C3D2201002F",
+			}},
+			wantValidDevices: []TestDevice{{
+				DeviceID: "00008020-00213C3D2201002F",
+			}},
+			wantDuplicatedDevices: nil,
+		},
+		{
+			name: "Duplicate devices",
+			deviceList: []TestDevice{
+				{
+					DeviceID: "00008020-00213C3D2201002F",
+				},
+				{
+					DeviceID: "0000802000213C3D2201002F",
+				},
+			},
+			wantValidDevices: []TestDevice{{
+				DeviceID: "00008020-00213C3D2201002F",
+			}},
+			wantDuplicatedDevices: []TestDevice{{
+				DeviceID: "0000802000213C3D2201002F",
+			}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotValidDevices, gotDuplicatedDevices := validateTestDevice(tt.deviceList)
+			require.Equal(t, tt.wantValidDevices, gotValidDevices, "normalizeTestDevices() validDevices")
+			require.Equal(t, tt.wantDuplicatedDevices, gotDuplicatedDevices, "normalizeTestDevices() duplicateDevices")
+		})
+	}
+}
+
+func TestTestDevice_IsEqualUDID(t *testing.T) {
+	tests := []struct {
+		name            string
+		UDID, otherUDID string
+		want            bool
+	}{
+		{
+			name:      "Same device",
+			UDID:      "00008020-00213C3D2201002F",
+			otherUDID: "00008020-00213C3D2201002F",
+			want:      true,
+		},
+		{
+			name:      "Same device with different casing and separators",
+			UDID:      "00008020-00213C3D2201002F",
+			otherUDID: "00008020 00213c3d2201002f",
+			want:      true,
+		},
+		{
+			name:      "Different devices",
+			UDID:      "00008020-00213C3D2201002F",
+			otherUDID: "00008020-00213C3D2201002G",
+			want:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsEqualUDID(tt.UDID, tt.otherUDID); got != tt.want {
+				t.Errorf("TestDevice.EqualsTo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
