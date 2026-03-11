817aff88ab172d1833c63f8d995feab51061223e
LaszloP <7979773+lpusok@users.noreply.github.com>
2026-03-11 22:44:10 +0100

    f

--- main.go
+++ main.go
@@ -416,7 +416,7 @@ alphanumeric characters.`)
 	if cfg.AppID != "" {
 		args = append(args, "--app", cfg.AppID)
 
-		//warn user if BundleID is also set
+		//warn user if BundleID is also set f
 		if cfg.BundleID != "" {
 			log.Warnf("AppID parameter specified, BundleID will be ignored")
 		}
